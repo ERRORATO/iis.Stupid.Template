a090860f1edf66f03b4d089b2345f44c80146d8b
ERRORATO <hickamconnor@gmail.com>
2026-03-05 20:09:48 -0600

    Implement custom mod menu with speed boost toggle
    Added a custom mod menu with speed boost functionality.

--- Plugin.cs
+++ Plugin.cs
@@ -1,15 +1,36 @@
-﻿using BepInEx;
+using BepInEx;
+using HarmonyLib;
+using UnityEngine;
 
-namespace StupidTemplate
+[BepInPlugin("com.connor.customgtagmenu", "Connor's Custom Mod Menu", "1.0.0")]
+public class CustomModMenu : BaseUnityPlugin
 {
-    [System.ComponentModel.Description(PluginInfo.Description)]
-    [BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
-    public class HarmonyPatches : BaseUnityPlugin
-    {
-        private void Awake() =>
-            GorillaTagger.OnPlayerSpawned(OnPlayerSpawned);
+    private bool showMenu = false;
+    private bool speedBoost = false;
+    private float speedMultiplier = 2f;
 
-        public void OnPlayerSpawned() =>
-            Patches.PatchHandler.PatchAll();
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F1)) // Toggle menu with F1 (adapt for VR if needed)
+        {
+            showMenu = !showMenu;
+        }
+    }
+
+    void OnGUI()
+    {
+        if (showMenu)
+        {
+            GUILayout.BeginArea(new Rect(10, 10, 200, 200));
+            GUILayout.Label("Connor's Mod Menu");
+            if (GUILayout.Button("Toggle Speed Boost"))
+            {
+                speedBoost = !speedBoost;
+                // Apply to player (use decompiled game refs)
+                // e.g., GorillaLocomotion.Player.Instance.maxArmLength *= speedBoost ? speedMultiplier : 1/speedMultiplier;
+            }
+            // Add more buttons: e.g., high jump, fly, etc.
+            GUILayout.EndArea();
+        }
     }
 }
